81b204f10dd9ca1a218618d84709df7fe96435d4
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-11-02 21:59:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ string = "Hello there, and how are you?"
 
 def reverse_each_word(string)
   string.split.collect do |word|
-    word.reverse
+    word.reverse = new_string
+    new_string.join(" ")
   end
 end
